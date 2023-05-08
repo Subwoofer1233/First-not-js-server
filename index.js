d75ea74396d32c129c9f611baefd9ef3e85ff538
subwoofer1233 <raicewheeler@yahoo.com>
2023-05-08 14:01:55 -0600

    get good

---
+++ index.js
@@ -0,0 +1,16 @@
+// const express = require("express");
+// const app = express()
+// const PORT = 3000;
+
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) =>{
+    res.send("Hello World");
+})
+
+app.listen(3000, () => {
+    console.log(`Test server running on port: ${PORT}`)
+})
